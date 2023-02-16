76717d3dd124b8c4cfdd6a84c2a1599e61f5cdcf
dcm323 <danielcalle323@gmail.com>
2023-02-16 08:16:19 -0500

    nt2 se modifica main con f prints y se crea archivo de condicionales

---
+++ condiciones.py
@@ -0,0 +1,1 @@
+#Condicionales con PYTHON
---
+++ main.py
@@ -0,0 +1,16 @@
+#DATOS PRIMITIVOS QUEMADOS  
+nombreUsuario="Juan Jose"
+edadUsuario=33
+estaturaUsuario=1.62
+yaDesayuno=True
+direccionUsuario=input("Digita tu direccion")
+
+#SALIDAS EN CONSOLA
+print("Hola tu nombre es "+nombreUsuario+" Bienvenido")
+print("Tu edad es: ",edadUsuario, "estas viejito")
+print(f"tu direccion es {direccionUsuario} vive cerca")
+
+numero1=int(input("Ingrese numero: "))
+numero2=int(input("Ingrese otro numero"))
+sumatoria=numero1+numero2
+print(f"la suma es: {sumatoria}")
